d5cd3d53b41ae685dd33f552b23455e20e2c1b8e
shunoks <oksana.shundryna@gmail.com>
2024-03-23 19:37:22 +0200

    test1

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'cypress';
+import {addMatchImageSnapshotPlugin} from '@simonsmith/cypress-image-snapshot/plugin';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/**/*.{cy,test}.{js,jsx}',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    defaultCommandTimeout: 4000,
+    retries: { runMode: 1, openMode: 1 },
+    scrollBehavior: 'center',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      addMatchImageSnapshotPlugin(on);
+    },
+  },
+});
